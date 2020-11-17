08a3f785a9334795c5e6084683ffd1f2bc6d9a98
Jordan Palmer <jordan@dmpnet.org>
2020-11-17 15:41:49 -0700

    Update HW6.m
    Put boundary conditions in place and made arrays for them.

--- HW6.m
+++ HW6.m
@@ -13,11 +13,26 @@ clear
 close all
 
 %test conditions 1 
-L = pi;
-D = 0.1;
-T = 10;
-F = 0;
+L = pi;     %space
+T = 10;     %time
+n = 10;
+dt = T/n;
+dx = L/n;
+k = 1;
+D = 0.1;    %coefficient
+%fx = sin(k*x);
 g0 = 0;
 gL = g0;
-fx = sin(k*x);
-uexact(x,t) = exp(-D*k^2*t)*sin(k*x);
+U0 = zeros(1, n);
+%uexact(x,t) = exp(-D*k^2*t)*sin(k*x);
+
+%boundary conditions
+U0(1) = g0;
+
+for x = 2:n-1
+    U0(x) = sin(k*x);
+end
+    U0(n) = gL;
+
+Un(1)=g0; 
+Un(n)=gL;
